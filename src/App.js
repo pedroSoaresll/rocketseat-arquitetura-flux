61c08e50402a3b254c76d645ffe31375c040fa88
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-01-15 18:21:13 -0300

    add provider from react redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
 
+import store from './store';
+
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes />
-    <GlobalStyle />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
